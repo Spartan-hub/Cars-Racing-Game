5e5fca44ed60dbb1a7c2471db10e8ca393accbb3
Spartan-hub <65815365+Spartan-hub@users.noreply.github.com>
2020-10-07 19:53:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,12 +9,12 @@ var car1Image, car2Image, car3Image, car4Image;
 var track;
 var backgroundImage;
 function preload() {
-  car1Image = loadImage("../images/car1.png");
-  car2Image = loadImage("../images/car2.png");
-  car3Image = loadImage("../images/car3.png");
-  car4Image = loadImage("../images/car4.png");
-  track = loadImage("../images/track.jpg");
-  backgroundImage = loadImage("../images/ground.png");
+  car1Image = loadImage("images/car1.png");
+  car2Image = loadImage("images/car2.png");
+  car3Image = loadImage("images/car3.png");
+  car4Image = loadImage("images/car4.png");
+  track = loadImage("images/track.jpg");
+  backgroundImage = loadImage("images/ground.png");
 }
 function setup() {
   createCanvas(displayWidth - 20, displayHeight - 20);
